35a27fd1911e22f93a6404798e36e6c6d47921aa
Jiao Xiangyu <mingxiruliu@gmail.com>
2016-08-24 19:21:27 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,44 +1,5 @@
 #!/bin/sh
 
-# check out the project
-echo "Please input the svn path of your Project!"
-read path
-echo $path
-echo "Please input your Project Name!"
-read proname
-echo $proname
-svn co $path /data/$proname
-
-# static-html-builder init
-buildDir="${proname}Builder"
-if [ ! -d "$buildDir" ]; then
-  git clone https://github.com/JrontEnd/static-html-builder $buildDir
-fi
-cd $buildDir
-npm install
-rm -rf app
-ln -s /data/$proname app
-grunt init
-bower install
-grunt build
-
-# cordova init
-cd ..
-cordovaDir="${proname}Cordova"
-if [ ! -d "$cordovaDir" ]; then
-  cordova create $cordovaDir
-  cd $cordovaDir
-  cordova platform add android
-else
-  cd $cordovaDir
-fi
-rm -rf www
-ln -s /data/$buildDir/dist www
-cordova build
-
-root@58be56bfad79:/data# cat build.sh
-#!/bin/sh
-
 # update the project
 svn update $1
 
